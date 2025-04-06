6c782da097b51e5c3807014b29c40dc1ee7722cd
Quesys-tech <29653819+Quesys-tech@users.noreply.github.com>
2025-04-06 19:02:51 +0900

    ci: update version in Cargo.lock alongside Cargo.toml

--- ci/bumper.py
+++ ci/bumper.py
@@ -8,8 +8,11 @@ if __name__ == "__main__":
         new_version = new_version[1:]
     
     project_dir = Path(__file__).parent.parent
+
+    # Chenge the version in Cargo.toml
     cargo_toml_path = project_dir / "Cargo.toml"
-    cargo_toml = tomlkit.load(cargo_toml_path)
+    with open(cargo_toml_path, "r") as f:
+        cargo_toml = tomlkit.load(f)
     
     pkg_name = cargo_toml["package"]["name"]
     print(f"Updating {pkg_name} version to {new_version}")
@@ -17,3 +20,16 @@ if __name__ == "__main__":
     cargo_toml["package"]["version"] = new_version
     with open(cargo_toml_path, "w") as f:
         tomlkit.dump(cargo_toml, f)
+
+    # Change the version in Cago.lock
+    cargo_lock_path = project_dir / "Cargo.lock"
+    with open(cargo_lock_path, "r") as f:
+        cargo_lock = tomlkit.load(f)
+
+    for pkg in cargo_lock["package"]:
+        if pkg["name"] == pkg_name:
+            pkg["version"] = new_version
+            break
+    with open(cargo_lock_path, "w") as f:
+        tomlkit.dump(cargo_lock, f)
+            
