28f25252561bd602ecee1030e4e6abb0ebd96c44
Queue_sys <29653819+Quesys-tech@users.noreply.github.com>
2025-05-18 00:54:32 +0900

    refactor: remove verbose mode and integrate tracing for logging

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use clap::Parser;
 use moon_phase::MoonPhase;
 use tokio::signal;
 use tokio::time::{sleep, Duration};
+use tracing::{debug, info};
+use tracing_subscriber;
 mod osc_sender;
 
 #[derive(Parser)]
@@ -30,19 +32,21 @@ struct Cli {
 async fn send_time(
     sender: &osc_sender::OscSender,
     time: &DateTime<Local>,
-    verbose: bool,
 ) -> Result<(), Box<dyn Error>> {
     let second_fraction = (time.second() as f64) / 60.0;
     let minute_fraction = (time.minute() as f64 + second_fraction) / 60.0;
     let hour_fraction = (time.hour() as f64 + minute_fraction) / 24.0;
 
-    sender.send( &(second_fraction as f32),"/avatar/parameters/DateTimeSecondFA")?;
-    sender.send( &(minute_fraction as f32),"/avatar/parameters/DateTimeMinuteFA")?;
-    sender.send( &(hour_fraction as f32),"/avatar/parameters/DateTimeHourFA")?;
+    sender.send(
+        &(second_fraction as f32),
+        "/avatar/parameters/DateTimeSecondFA",
+    )?;
+    sender.send(
+        &(minute_fraction as f32),
+        "/avatar/parameters/DateTimeMinuteFA",
+    )?;
+    sender.send(&(hour_fraction as f32), "/avatar/parameters/DateTimeHourFA")?;
 
-    if verbose {
-        println!("{}:{}:{}", time.hour(), time.minute(), time.second());
-    }
     Ok(())
 }
 
@@ -88,25 +92,23 @@ async fn test_calc_moon_phase() {
     }
 }
 
-async fn tick_watch(cli: &Cli, sender: &osc_sender::OscSender) -> Result<(), Box<dyn Error>> {
+async fn tick_watch(sender: &osc_sender::OscSender) -> Result<(), Box<dyn Error>> {
     let now = Local::now();
-    send_time(sender, &now, cli.verbose).await?;
+    send_time(sender, &now).await?;
     let moon_phase = calc_moon_phase(&now).await;
     send_moon_phase(sender, moon_phase).await?;
 
     Ok(())
 }
 
-async fn update_second_change(cli: Cli, sender: osc_sender::OscSender) {
+async fn update_second_change(sender: osc_sender::OscSender) {
     loop {
         let now = Local::now();
         let sub_second = now.timestamp_subsec_nanos();
         let sleep_duration = Duration::from_nanos(1_000_000_000 - sub_second as u64);
-        if cli.verbose {
-            println!("Sleeping for {}ms", sleep_duration.as_millis());
-        }
+        debug!("Sleeping for {}ms", sleep_duration.as_millis());
         sleep(sleep_duration).await;
-        tick_watch(&cli, &sender).await.unwrap();
+        tick_watch(&sender).await.unwrap();
     }
 }
 
@@ -114,10 +116,16 @@ async fn update_second_change(cli: Cli, sender: osc_sender::OscSender) {
 async fn main() {
     let cli = Cli::parse();
 
-    if cli.verbose {
-        println!("Verbose mode enabled");
-    }
-    println!("Destination port: {}:{}", cli.address, cli.port);
+    tracing_subscriber::fmt()
+        .with_max_level(if cli.debug {
+            tracing::Level::DEBUG
+        } else {
+            tracing::Level::INFO
+        })
+        .init();
+
+    debug!("Verbose mode enabled");
+    info!("Destination port: {}:{}", cli.address, cli.port);
 
     let sender = osc_sender::OscSender::new(Ipv4Addr::new(127, 0, 0, 1), 0, cli.address, cli.port);
     match cli.demo {
@@ -125,22 +133,22 @@ async fn main() {
             tokio::spawn(demo_mode(sender));
         }
         false => {
-            tokio::spawn(update_second_change(cli, sender));
+            tokio::spawn(update_second_change(sender));
         }
     }
-    println!("Press Ctrl-C to exit");
+    info!("Press Ctrl-C to exit");
     let mut sigint_handler = signal::windows::ctrl_c().unwrap();
     sigint_handler.recv().await;
-    println!("Exiting...");
+    info!("Exiting...");
 }
 
 async fn demo_mode(sender: osc_sender::OscSender) {
     let display_time = Local.with_ymd_and_hms(2017, 2, 1, 10, 8, 42).unwrap(); // https://museum.seiko.co.jp/knowledge/trivia01/
 
-    println!("Display mode: fixed at {}", display_time);
+    info!("Display mode: fixed at {}", display_time);
 
     loop {
-        send_time(&sender, &display_time, true).await.unwrap();
+        send_time(&sender, &display_time).await.unwrap();
         sleep(Duration::from_secs(1)).await;
     }
 }
