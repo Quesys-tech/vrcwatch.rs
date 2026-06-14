0be9058ed29f11d9b0d3101907a88cb478189723
Queue_sys <29653819+Quesys-tech@users.noreply.github.com>
2026-06-14 22:22:01 +0900

    wip: install and uninstall mock

--- src/ovr_manifest.rs
+++ src/ovr_manifest.rs
@@ -2,7 +2,7 @@ extern crate openvr;
 
 const OVR_APP_KEY: &str = "tech.qsys.vrcwatch";
 
-pub async fn status(){
+pub async fn status() {
     let context = unsafe { openvr::init(openvr::ApplicationType::Utility) }
         .expect("Unable in OpenVR initialization");
     let mut application = context
@@ -16,7 +16,7 @@ pub async fn status(){
             } else {
                 println!("VRCWatch is NOT installed in SteamVR.");
             }
-        },
+        }
         Err(e) => {
             eprintln!("Error checking VRCWatch installation status: {:?}", e);
         }
@@ -24,9 +24,42 @@ pub async fn status(){
 }
 
 pub async fn install() {
-    println!("Installation functionality is not implemented yet.");
+    let context = unsafe { openvr::init(openvr::ApplicationType::Utility) }
+        .expect("Unable in OpenVR initialization");
+    let mut application = context
+        .application()
+        .expect("Unable to get OpenVR application");
+    match application.is_application_installed(OVR_APP_KEY) {
+        Ok(installed) => {
+            if installed {
+                println!("VRCWatch is already installed in SteamVR.");
+            } else {
+                todo!("Installation functionality is not implemented yet.");
+            }
+        }
+        Err(e) => {
+            eprintln!("Error checking VRCWatch installation status: {:?}", e);
+        }
+    }
 }
 
 pub async fn uninstall() {
-    println!("Uninstallation functionality is not implemented yet.");
+    let context = unsafe { openvr::init(openvr::ApplicationType::Utility) }
+        .expect("Unable in OpenVR initialization");
+    let mut application = context
+        .application()
+        .expect("Unable to get OpenVR application");
+
+    match application.is_application_installed(OVR_APP_KEY) {
+        Ok(installed) => {
+            if installed {
+                todo!("Uninstallation functionality is not implemented yet.");
+            } else {
+                println!("VRCWatch is not installed in SteamVR.");
+            }
+        }
+        Err(e) => {
+            eprintln!("Error checking VRCWatch installation status: {:?}", e);
+        }
+    }
 }
