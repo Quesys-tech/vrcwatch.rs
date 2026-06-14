95ab767c8bcbf678a95d1e736549648a8ad04c58
Queue_sys <29653819+Quesys-tech@users.noreply.github.com>
2026-06-14 21:00:00 +0900

    feat: add commands for checking installation and managing VRCWatch in SteamVR

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use clap::{Parser, Subcommand};
 
 mod osc_sender;
+mod ovr_install;
 mod run_watch;
 
 #[derive(Subcommand)]
 enum Command {
+    /// Run VRCWatch with specified options
     Run(run_watch::RunArgs),
+    /// Check if VRCWatch is installed in SteamVR
+    Status,
+    /// Install VRCWatch in SteamVR (not implemented yet)
+    Install,
+    /// Uninstall VRCWatch from SteamVR (not implemented yet)
+    Uninstall,
 }
 
 #[derive(Parser)]
@@ -20,6 +28,24 @@ async fn main() {
         Some(Command::Run(args)) => {
             run_watch::run_watch(&args).await;
         }
+        Some(Command::Status) => {
+            let installed = ovr_install::ovr_manifest_status().await;
+            match installed {
+                Ok(true) => println!("VRCWatch is installed."),
+                Ok(false) => println!("VRCWatch is not installed."),
+                Err(e) => eprintln!("Error checking installation status: {:#?}", e),
+            }
+        }
+        Some(Command::Install) => {
+            println!("Installation functionality is not implemented yet.");
+        }
+        Some(Command::Uninstall) => match ovr_install::ovr_manifest_status().await {
+            Ok(true) => {
+                println!("Uninstallation functionality is not implemented yet.")
+            }
+            Ok(false) => println!("VRCWatch is not installed, so it cannot be uninstalled."),
+            Err(e) => eprintln!("Error checking installation status: {:#?}", e),
+        },
         None => {
             run_watch::run_watch(&run_watch::RunArgs::default()).await;
         }
---
+++ src/ovr_install.rs
@@ -0,0 +1,18 @@
+extern crate openvr;
+
+use openvr::errors::VRApplicationError;
+
+const OVR_APP_KEY: &str = "tech.qsys.vrcwatch";
+
+pub async fn ovr_manifest_status() -> Result<bool, VRApplicationError> {
+    let context = unsafe { openvr::init(openvr::ApplicationType::Utility) }
+        .expect("Unable in OpenVR initialization");
+    let mut application = context
+        .application()
+        .expect("Unable to get OpenVR application");
+
+    match application.is_application_installed(OVR_APP_KEY) {
+        Ok(installed) => Ok(installed),
+        Err(e) => Err(e.into()),
+    }
+}
